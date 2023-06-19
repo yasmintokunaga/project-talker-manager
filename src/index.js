db5d2985b11c8f64469b09b3295bb297fae4c1e8
yasmintokunaga <yasmintkng@gmail.com>
2023-06-19 16:44:40 -0300

    start the project, feat(REQ01): done the endpoint GET /talker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const { join } = require('path');
 
 const app = express();
 app.use(express.json());
@@ -13,4 +15,11 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (req, res) => {
+  const path = './talker.json';
+  const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+  const talkers = JSON.parse(contentFile);
+  return res.status(200).json(talkers); 
 });
