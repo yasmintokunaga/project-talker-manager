5f05d138a639a01c624dfc41b853a05a9cbf8971
yasmintokunaga <yasmintkng@gmail.com>
2023-06-19 18:09:11 -0300

    feat (REQ04): Create the validation login

--- src/index.js
+++ src/index.js
@@ -28,17 +28,34 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
-app.post('/login', (req, res) => {
-  const requiredProperties = ['email', 'password'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    const generateRandomToken = Math.random().toString(16).substr(2)
-      + Math.random().toString(16).substr(2);
-    const token = generateRandomToken
-      .split('')
-      .splice(1, 16)
-      .join('');
-    res.status(200).json({ token });
-  } else {
-    res.sendStatus(400);
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+  if (!email.includes('.com') || !email.includes('@') || email === '@.com') {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+app.post('/login', validateEmail, validatePassword, (req, res) => {
+  const generateRandomToken = Math.random().toString(16).substr(2)
+    + Math.random().toString(16).substr(2);
+  const token = generateRandomToken
+    .split('')
+    .splice(1, 16)
+    .join('');
+  res.status(200).json({ token });
 });
